cf3c5d565bd7d91ce58b0ced4cf05c2e1c0ecd06
ChangHuang <ch1075@scarletmail.rutgers.edu>
2023-09-06 17:08:33 +0800

    first

---
+++ LearningNotes/src/main/java/com/example/learningnotes/LearningNotesApplication.java
@@ -0,0 +1,13 @@
+package com.example.learningnotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningNotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningNotesApplication.class, args);
+	}
+
+}
